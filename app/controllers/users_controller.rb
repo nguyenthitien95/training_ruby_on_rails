268c7481581c7e3c54dcafd36c983da84b9bb467
nvt <thanhbk1.16@gmail.com>
2019-03-25 19:21:03 +0700

    dsada

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
 
   def show
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -18,8 +19,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   private
+
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
